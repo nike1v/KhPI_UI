450d6b5272a7bae480d468c8469c50dd815bf0b1
nike1v <dgygurdenok@gmail.com>
2020-12-23 13:48:24 +0200

    update

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    name: ''
+  }
+
+  fetching = async () => {
+    try {
+      let res = await (await fetch('https://localhost:3000/')).json();
+      console.log(res);
+      this.setState({
+        name: res
+      })
+    }catch (error){
+      console.log(error);
+    } 
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Test it out</Text>
+        <Button
+          onPress={this.fetching}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Text>{this.state.name}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
